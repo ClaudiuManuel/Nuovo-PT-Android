e53124079fc817a69b8f7f7799524a245064936b
Claudiu Calian <calian.claudiu@yahoo.com>
2021-03-06 23:52:11 +0200

    Implemented home screen desgin&functionality.Changed how navigation between fragments is achieved all throughout the app(WIP)

---
+++ app/src/main/java/com/example/nuovo_pt/ui/GridSpacing.java
@@ -0,0 +1,4 @@
+package com.example.nuovo_pt.ui;
+
+public class GridSpacing {
+}
---
+++ app/src/main/java/com/example/nuovo_pt/ui/HomeStartingPoint.java
@@ -0,0 +1,35 @@
+package com.example.nuovo_pt.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.nuovo_pt.R;
+
+public class Home extends Fragment {
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public Home() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
